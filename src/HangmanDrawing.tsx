de23cf90f501e533c70e9183af78a99d9da395f2
ArNGiH <aryannayar47@gmail.com>
2024-05-20 00:04:56 +0530

    Added typescript functionality to components

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -84,19 +84,44 @@ const HEAD = (
       }}
     />
   )
-
-
-export function HangmanDrawing(){
-    return( <div style={{ position:"relative"}} >
-        
-    
-    <div style={{height:"50px",width:"10px",background:"black",top:0,right:0, position:"absolute"}}/>
-    <div style={{height:"10px",width:"200px",background:"black",marginLeft:"120px" }}/>
-    <div style={{height:"400px",width:"10px",background:"black",marginLeft:"120px" }}/>
-     <div style={{height:"10px", width:"250px",background:"black"}}/>
-   
-   
-   
-   </div>
-)
-}
+  
+  const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+  
+  type HangmanDrawingProps = {
+    numberOfGuesses: number
+  }
+  
+  export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+    return (
+      <div style={{ position: "relative" }}>
+        {BODY_PARTS.slice(0, numberOfGuesses)}
+        <div
+          style={{
+            height: "50px",
+            width: "10px",
+            background: "black",
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+        />
+        <div
+          style={{
+            height: "10px",
+            width: "200px",
+            background: "black",
+            marginLeft: "120px",
+          }}
+        />
+        <div
+          style={{
+            height: "400px",
+            width: "10px",
+            background: "black",
+            marginLeft: "120px",
+          }}
+        />
+        <div style={{ height: "10px", width: "250px", background: "black" }} />
+      </div>
+    )
+  }
