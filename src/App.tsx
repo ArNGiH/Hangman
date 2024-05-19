0c6dc1126ebc8d22582efc65832742a1b461dbed
ArNGiH <aryannayar47@gmail.com>
2024-05-19 22:59:24 +0530

    Added hangman drawing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+
+import { useState } from 'react'
+import words from "./wordlist.json"
+import { HangmanDrawing } from './HangmanDrawing'
+import { HangmanWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
+
 
 function App() {
-  return <h1>Hi</h1>
+  const [wordtoguess,setwordtoguess]=useState(()=>{
+  return words[Math.floor(Math.random()*words.length)]
+})
+
+const[guessedletters,setguessedletters]= useState<string[]>([])
+
+console.log(wordtoguess);
+
+return( <div style={
+  {
+    maxWidth:"800px",
+    display:'flex',
+    flexDirection:"column",
+    gap:"2rem",
+    margin:"0 auto",
+    alignItems:"center"
+
+
+  }}>
+
+<div style={{
+  fontSize:"2rem",
+  textAlign:"center"
+}}>
+  Lose 
+  Win
+</div>
+
+<HangmanDrawing/>
+<HangmanWord/>
+<Keyboard/>
+
+
+
+</div>
+)
+
 }
 
-export default App
+
+
+export default App;
